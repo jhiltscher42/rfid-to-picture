9a3956e843af2ea434cd2cf6cdf1fb4cc8ae1d7b
Justin Hiltscher <j.hiltscher@gmail.com>
2015-02-04 13:59:14 -0600

    created got_id event

--- jscript/index.js
+++ jscript/index.js
@@ -2,4 +2,19 @@ $(function(){
 
 	var current_id='';
 
+	$("body").keydown(function(event){
+		if (event.which===13){
+			//enter was pressed.  current_id is collected; do what we're doing with it
+			//then clear it out.
+			$(this).trigger("got_id",[current_id]);
+			current_id="";
+		}
+		else
+		{
+			current_id=current_id+String.fromCharCode(event.which);
+		}
+	});
+
+
+	$("body").on("got_id",console.log.bind(console));
 });
